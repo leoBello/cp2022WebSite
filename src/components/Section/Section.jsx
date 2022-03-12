168b83ee8583683643a12f6b6549b1193617d049
leoBello <leobello.wd@gmail.com>
2022-03-12 23:51:14 +0100

    add responsive

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,15 +1,19 @@
 import "./section.scss";
 
 const SectionTitle = (props) => {
-  return <h2 className={`section-title ${props.center && 'section-title-center'}`}>{props.title}</h2>;
+  return <h2 className={`section-title ${props.center ? 'section-title-center' : ''}`}>{props.title}</h2>;
 };
 
 const Section = (props) => {
   return (
     <section className="section-container" id={props.id}>
+      <div className="section-left">
+      </div>
       <div className="section-text-container">
       <SectionTitle title={props.title} center={props.center}/>
       {props.children}
+      </div>
+      <div className="section-right">
       </div>
     </section>
   );
@@ -22,8 +26,8 @@ const SectionText = (props) => (
 const SectionPrecepte = (props) => {
   return (
     <div className="precepte-container">
-      <div className="precepte-title"
-      > <h3>{props.title}</h3>
+      <div className="precepte-title">
+        <h3>{props.title}</h3>
       </div>
       <div className="precepte-text">
         <p>{props.text}</p>
