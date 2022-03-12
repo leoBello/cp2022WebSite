ec3ad2e124f78c493e3a110c469c00533c54f574
leoBello <leobello.wd@gmail.com>
2022-03-12 20:44:49 +0100

    manage responsive

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,28 +5,32 @@ const Hero = (props) => {
     <>
       <div className="hero-container">
         <div className="content">
-        <h2 className="">{props.title.split('').map((letter, index) => {
-          return <span key={index} className="letter">{letter}</span>
-        })}</h2>
-        <h2 className="">{props.title.split('').map((letter, index) => {
-          return <span key={index} className="letter">{letter}</span>
-        })}</h2>
+          <h2 className="">
+            {props.title.split("").map((letter, index) => {
+              return (
+                <span key={index} className="letter">
+                  {letter}
+                </span>
+              );
+            })}
+          </h2>
+          <h2 className="">
+            {props.title.split("").map((letter, index) => {
+              return (
+                <span key={index} className="letter">
+                  {letter}
+                </span>
+              );
+            })}
+          </h2>
         </div>
         <div className="hero-text">
           <h3>{props.text}</h3>
         </div>
-        <div className="hero-button-container">
-          <a className="hero-button" href={props.buttonLink}>
-            {props.buttonText}
-          </a>
-        </div>
+        
       </div>
-       
-        <div className="scroll-down ">
-        {/* {!props.showScrollDown && <>
-          <span className="chevron bottom chevron-1"></span>
-          <span className="chevron bottom chevron-2"></span></>} */}
-        </div>
+
+      <div className="scroll-down"></div>
     </>
   );
 };
