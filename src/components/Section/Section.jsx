3df54e9a16ad6b82bc65975959e291863ed568d8
leoBello <leobello.wd@gmail.com>
2022-04-07 10:31:08 +0200

    center costumes guidelines text

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,29 +1,32 @@
 import "./section.scss";
 
 const SectionTitle = (props) => {
-  return <h2 className={`section-title ${props.center ? 'section-title-center' : ''}`}>{props.title}</h2>;
+  return (
+    <h2
+      className={`section-title ${props.center ? "section-title-center" : ""}`}
+    >
+      {props.title}
+    </h2>
+  );
 };
-
-
 
 const Section = (props) => {
   return (
     <section className="section-container" id={props.id}>
-      <div className="section-left">
-
-      </div>
+      <div className="section-left"></div>
       <div className="section-text-container">
-      <SectionTitle title={props.title} center={props.center}/>
-      {props.children}
+        <SectionTitle title={props.title} center={props.center} />
+        {props.children}
       </div>
-      <div className="section-right">
-      </div>
+      <div className="section-right"></div>
     </section>
   );
 };
 
 const SectionText = (props) => (
-    <p className="section-text" {...props}>{props.text}</p>
+  <p className="section-text" {...props}>
+    {props.text}
+  </p>
 );
 
 const SectionPrecepte = (props) => {
@@ -39,14 +42,17 @@ const SectionPrecepte = (props) => {
   );
 };
 
-const  SectionCostume = (props) => {
+const SectionCostume = (props) => {
   return (
     <div className="costume-container">
-      <h3 className="costume-day">{props.day}</h3>
-      <p className="costume-title">{props.title}</p>
+      <div className="costume-day">
+        <h3>{props.day}</h3>
+      </div>
+      <div className="costume-title">
+        <p className="">{props.title}</p>
+      </div>
     </div>
   );
 };
-
 
 export { Section, SectionText, SectionPrecepte, SectionCostume };
